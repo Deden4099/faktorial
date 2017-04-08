5b0570153db9ed0d3840c393a0e8c9bf0e8eb964
Deden <dedennursyadid@gmail.com>
2017-04-08 12:25:44 +0700

    Add files via upload

---
+++ faktorial/src/faktorial/Faktorial.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faktorial;
+
+/**
+ *
+ * @author Praktikan
+ */
+class fatorial {
+  public static void main(String[] args) {
+    for (int i = 1; i < 10; i += 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+
+    for (int i = 7; i > 0; i -= 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+  }
+}
